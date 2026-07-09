89c28615ab2def329dfc86e6cdc7bb69fb7103d0
kenzy-123 <kinzem68@gmail.com>
2026-07-09 18:53:47 +0300

    ki

--- routes/contestRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// أي routes هتضيفها هنا بعدين، مثلاً:
-// router.get('/', (req, res) => res.json({ message: 'test' }));
-
-module.exports = router;
